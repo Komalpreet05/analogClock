74d2cc75607b0810efebe52a3ef682d59a90161c
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2023-12-27 03:28:57 -0500

    final version

--- index.js
+++ index.js
@@ -18,3 +18,65 @@ function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360);
 }
 setClock();
+
+// let second = document.getElementById("two");
+// second.addEventListener("click", () => {
+//     alert("clicked");
+// })
+
+
+//default
+const clock = document.getElementById("clock");
+clock.style.backgroundColor = "black";
+clock.style.color = "white";
+document.body.style.background = "aqua";
+
+//color selection
+// let first = document.getElementById("one");
+// first.addEventListener("click", () => {
+//     clock.style.backgroundColor = "yellow";
+//     clock.style.color = "black";
+//     document.body.style.background = "aqua";
+// })
+
+//dropdwon access
+let part = document.querySelector("#part");
+
+console.log(part.value);
+// part.addEventListener("click", () => {
+//     console.log("ch", part.value);
+
+// })
+// console.log(part.value);
+
+//test
+let head = document.getElementById("head");
+let numbersAll = document.querySelectorAll(".number");
+let colorPick = document.getElementById("favcolor");
+colorPick.addEventListener("input", () => {
+    console.log("ok", colorPick.value);
+    //clock.style.backgroundColor = colorPick.value;
+    if (part.value === "clockBackground") {
+        backColorChange(clock);
+    }
+    if (part.value === "webBackground") {
+        backColorChange(document.body);
+    }
+    if (part.value === "numbers") {
+        numbersAll.forEach(num => {
+            colorChange(num);
+        })
+    }
+    if (part.value === "border") {
+        clock.style.border = `5px solid ${colorPick.value}`;
+    }
+
+})
+
+function backColorChange(partColor) {
+    partColor.style.backgroundColor = colorPick.value;
+}
+
+function colorChange(partColor) {
+    partColor.style.color = colorPick.value;
+}
